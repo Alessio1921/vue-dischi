9d9084f18aaa0625b400a8d11ad40587855fe4c7
Alessio1921 <caruso.alessio1999@gmail.com>
2022-03-30 17:23:26 +0200

    added loading screen

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,36 +1,45 @@
 <template>
   <main class="pb-5">
     <div class="container">
-      <div class="row row-cols-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-6">
+      <div class="row row-cols-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-6" v-if="elementList">
         <div class="col" v-for="(element,index) in elementList" :key="index">
           <Card 
             :card="element"
           />
         </div>
       </div>
+      <div class="loader d-flex justify-content-center" v-else> <Loader /></div>
     </div>
   </main>
 </template>
 
 <script>
 import Card from './Card.vue'
+import Loader from './Loader.vue'
 import axios from 'axios'
 
 export default {
-  components: { Card },
+  components: { Card,
+  Loader
+  },
   name: 'CardList',
   data:function(){
     return{
       elementList:null
     }
   },
-  mounted() {
-    axios
+  methods:{
+    getApi(){
+      axios
       .get('https://flynn.boolean.careers/exercises/api/array/music')
       .then(response => (this.elementList = response.data.response))
       .catch(error => {
         console.log(error)
       })
+    }
+  },
+  mounted() {
+    setTimeout(this.getApi,2000)
   }
   
 }
@@ -41,5 +50,8 @@ export default {
   main{
     background-color: rgb(33,45,58);
     height: 100%;
+    .loader{
+      height: 100vh;
+    }
   }
 </style>
